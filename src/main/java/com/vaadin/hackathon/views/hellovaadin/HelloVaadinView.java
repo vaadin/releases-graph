8fab86c75a721074f2f47d53d5792b0715c047ff
Sujoy <sujoy@vaadin.com>
2023-02-25 18:03:59 +0200

    view and export raw data

--- src/main/java/com/vaadin/hackathon/views/hellovaadin/HelloVaadinView.java
+++ src/main/java/com/vaadin/hackathon/views/hellovaadin/HelloVaadinView.java
@@ -1,15 +1,27 @@
 package com.vaadin.hackathon.views.hellovaadin;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.charts.events.PointClickEvent;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.StreamRegistration;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.hackathon.git.GitHubService;
 import com.vaadin.hackathon.git.GitService;
 import com.vaadin.hackathon.git.MajorVersionInfo;
@@ -56,11 +68,73 @@ public class HelloVaadinView extends VerticalLayout {
         });
         radioGroup.setValue(ChartChoice.BY_RELEASE_COUNT);
 
-        this.add(radioGroup, this.chartArea);
+        final var header = new HorizontalLayout();
+        header.addAndExpand(radioGroup);
+        header.add(this.viewButton(), this.exportButton());
+        header.addClassName(LumoUtility.AlignItems.CENTER);
+
+        this.add(header, this.chartArea);
 
         final MajorVersionInfo majorVersionInfo = this.consolidatedVersionsInfo.get(0);
         this.versionsTimelineChart = new VersionsTimelineChart(this.gitHubService, majorVersionInfo);
         this.add(this.versionsTimelineChart);
+    }
+
+    private Button exportButton() {
+        return new Button("Export Raw Data", event -> {
+            final StreamResource resource = new StreamResource("vaadin_versions.xlsx",
+                                                               () -> new ByteArrayInputStream(this.prepareSpreadsheet().toByteArray()));
+            final StreamRegistration registration = VaadinSession.getCurrent().getResourceRegistry().registerResource(resource);
+            UI.getCurrent().getPage().open(registration.getResourceUri().toString());
+        });
+    }
+
+    private Button viewButton() {
+        return new Button("View Raw Data", event -> {
+            final Dialog dialog = new Dialog();
+            dialog.setHeaderTitle("Vaadin all released version details");
+            dialog.add(this.createSpreadsheet());
+            dialog.setWidth("400px");
+            dialog.setHeight("600px");
+            dialog.open();
+        });
+    }
+
+    private ByteArrayOutputStream prepareSpreadsheet() {
+
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final Spreadsheet spreadsheet = this.createSpreadsheet();
+        try {
+            spreadsheet.write(out);
+        } catch (final IOException e) {
+            Notification.show("Error exporting charts");
+        }
+        return out;
+    }
+
+    private Spreadsheet createSpreadsheet() {
+        final Spreadsheet spreadsheet = new Spreadsheet();
+        spreadsheet.setHeightFull();
+        spreadsheet.setWidthFull();
+
+        final int versionColumn = 0;
+        final int releasedOnColumn = 1;
+
+        final AtomicInteger rowCount = new AtomicInteger(0);
+        spreadsheet.createCell(rowCount.get(), versionColumn, "Version");
+        spreadsheet.createCell(rowCount.getAndIncrement(), releasedOnColumn, "Released On");
+
+        this.consolidatedVersionsInfo.stream()
+                                     .map(MajorVersionInfo::getAllVersions)
+                                     .flatMap(List::stream)
+                                     .forEach(item -> {
+                                         spreadsheet.createCell(rowCount.get(), versionColumn, item.getVersion());
+                                         spreadsheet.createCell(rowCount.getAndIncrement(), releasedOnColumn, item.getReleasedOn().toLocalDateTime());
+                                     });
+        spreadsheet.autofitColumn(versionColumn);
+        spreadsheet.autofitColumn(releasedOnColumn);
+        spreadsheet.setSheetName(spreadsheet.getActiveSheetIndex(), "Vaadin Versions");
+        return spreadsheet;
     }
 
     private VersionsBarChart chartByReleaseCount() {
