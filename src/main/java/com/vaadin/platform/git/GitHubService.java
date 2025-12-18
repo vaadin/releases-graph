3cb2aaca2411c38366ba4c04b9524de58a86e579
Manolo Carrasco <manolo@vaadin.com>
2025-12-18 22:15:46 +0100

    Update to Vaadin 25.0.0

--- src/main/java/com/vaadin/platform/git/GitHubService.java
+++ src/main/java/com/vaadin/platform/git/GitHubService.java
@@ -13,8 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class GitHubService {
